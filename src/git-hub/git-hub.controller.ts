d75afa753fc3254aaa78e3105a1c2697a3035d1c
Emanuel <bolzicoemanuel@gmail.com>
2022-06-21 20:21:47 -0300

    controller function

--- src/git-hub/git-hub.controller.ts
+++ src/git-hub/git-hub.controller.ts
@@ -8,7 +8,16 @@ export class GitHubController {
   @Get()
   async getHistory() {   
     let response: any = await this.GitHubService.getHistory()
-    console.log(response.data)
-    return response.data
+    let commits: any = []
+    commits = response.data.map(commit => {
+      let data = commit.commit
+      return {
+        name: data.author.name,
+        date: data.author.date,
+        message: data.message,
+        id: data.tree.sha
+      }
+    })
+    return commits
   }
 }
